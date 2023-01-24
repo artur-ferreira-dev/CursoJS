bf63716b0679a55bf818cff55375af9f58722bcd
Artur Fonseca Ferreira <115838267+artur-ferreira-dev@users.noreply.github.com>
2023-01-24 14:28:14 -0300

    Create enquanto.js

---
+++ Aula006/enquanto.js
@@ -0,0 +1,11 @@
+// Utilização do 'while' como estrutura de repetição com condição no início
+var c = 1;
+while ( c <= 6) {
+    console.log(`Passo ${c}`);
+    c++;
+}
+// Utilização do 'while' como estrutura de repetição com condição no final
+do {
+    console.log(`Passo ${c}`);
+    c++;
+} while (c <= 12);
